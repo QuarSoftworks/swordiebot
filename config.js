79ac53af90826e836fd008f9c726601027db81e5
silverscripts11@gmail.com <silverscripts11@gmail.com>
2022-05-29 03:32:03 +0200

    yo

---
+++ config.js
@@ -0,0 +1,13 @@
+module.exports = {
+ "verification": "",
+ "description": "",
+ "domain": "", // This Domain need to be without https:// or http:// (eg: hydrabot.fun not https://hydrabot.fun)
+ "google_analitics": "", 
+ "token": "OTY5MjQ5MTU0NDA2Njk5MDk5.GqVKHJ.166uwr_eHvpLT22e2YOQBHdoTHVfFH_q7aMaCs",
+ "https":"https://",
+ "port":"5003",
+ "client_id":"969249154406699099",
+ "secret":"-wCbNPxNcVbTN7UE74Y8vK4FIEJ4Jcqg",
+ "support_server_link":"https://discord.gg/QqvujPRE",
+ "bot_name":"Swordie"
+}
